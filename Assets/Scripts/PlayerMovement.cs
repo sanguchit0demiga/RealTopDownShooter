8c0e8dc1a9e6254049016544233adc9c88b26dd2
sanguchit0demiga <camilarocio005@gmail.com>
2025-06-03 22:25:43 -0300

    player and camera movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
+
+public class PlayerController : MonoBehaviour
+{
+    
+
+    public Transform targetObject;
+
+    public float gravityForce;
+    [Range(1f, 10f)]
+    public float movementSpeed;
+
+    private float verticalVelocity;
+    public Vector2 movementInput;
+    public Vector2 mousePosition;
+    public Vector3 lookTarget;
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (!characterController.isGrounded)
+        {
+
+            verticalVelocity = gravityForce * Time.deltaTime;
+        }
+
+        float movementX = (movementInput.x * movementSpeed * Time.deltaTime);
+        float movementZ = (movementInput.y * movementSpeed * Time.deltaTime);
+
+        Vector3 finalMovement = new Vector3(movementX, verticalVelocity, movementZ);
+
+        characterController.Move(finalMovement);
+
+        lookTarget.y = transform.position.y;
+        transform.LookAt(lookTarget);
+
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
+        movementInput = context.ReadValue<Vector2>();
+        Debug.Log("Movement Input: " + movementInput);
+    }
+
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+
+        mousePosition = context.ReadValue<Vector2>();
+        Debug.Log("Mouse Postion: " + mousePosition);
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        if (groundPlane.Raycast(ray, out float enter))
+        {
+            lookTarget = ray.GetPoint(enter);
+
+        }
+    }
+
+}
