360bd50a8ffa2cf184f92acf1cb99821e514d6f6
sanguchit0demiga <camilarocio005@gmail.com>
2025-06-12 23:02:14 -0300

    map

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementController : MonoBehaviour
+{
+   
+
+    public Transform playerTransform; 
+    private NavMeshAgent navMeshAgent; 
+    [SerializeField] private float refreshRate = 0.5f; 
+    private void Awake()
+    {
+        playerTransform = GameObject.FindWithTag("Player")?.transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        InvokeRepeating("FindPlayer", 0f, refreshRate); 
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    public void FindPlayer()
+    {
+
+        navMeshAgent.SetDestination(playerTransform.position); 
+
+    }
+
+
+
+}
